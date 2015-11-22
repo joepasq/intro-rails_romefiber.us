081db9a61b5083847986c7e997d35017d4043a86
Joe Pasqualetti <public_github@joepasq.com>
2015-11-22 16:11:12 -0500

    Show alerts when clicking a plain_pages#index entry to demonstrate javascript.
    - Switch plain_pages.js from coffee script to plain javascript.
    - Switch PlainPagesController from 4 spaces to 2 spaces indentation.
    - Try to randomize entries displayed, as long as there are enough in the database.
    - Expand PlainPages#index list of entries and add javascript call to each entry there.

---
+++ app/assets/javascripts/plain_pages.js
@@ -0,0 +1,11 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  console.log("jQuery document ready on a plain_page.");  
+});
+
+function entry_detail(title) {
+  /* Do something more creative later */
+  window.alert(title);
+}
--- app/controllers/plain_pages_controller.rb
+++ app/controllers/plain_pages_controller.rb
@@ -1,11 +1,17 @@
 class PlainPagesController < ApplicationController
-    def index
-        @time = Time.now
-        @entries = Entry.find_each
-        
-        @new_entry = Entry.new
-        @message = ["What would you do with fiber?",
-                    "What can you build with gigabit internet?",
-                    "Why do you need better internet?"].shuffle.first
+  def index
+    if Entry.count > 20 
+      # Randomize entries if we have many.
+      random = 1 + Random.rand(Entry.count)
+      @entries = Entry.limit(10).offset(random)
+    else 
+      # Without enough to be random, grab them all.
+      @entries = Entry.find_each
     end
+    
+    @new_entry = Entry.new
+    @message = ["What would you do with fiber?",
+      "What can you build with gigabit internet?",
+      "Why do you need better internet?"].shuffle.first
+  end
 end
