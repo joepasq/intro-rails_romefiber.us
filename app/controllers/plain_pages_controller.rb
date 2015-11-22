27d0422b378dd5abc2fd7d1328c86ac71e4b9df8
Joe Pasqualetti <public_github@joepasq.com>
2015-11-21 20:27:30 -0500

    Add new-entry form on root PlainPages.
    - Display shuffled messages when entering form.
    - Start with a new entry, and then the form will submit it (client-side) to EntiresController#create.

--- app/controllers/plain_pages_controller.rb
+++ app/controllers/plain_pages_controller.rb
@@ -2,5 +2,10 @@ class PlainPagesController < ApplicationController
     def index
         @time = Time.now
         @entries = Entry.find_each
+        
+        @new_entry = Entry.new
+        @message = ["What would you do with fiber?",
+                    "What can you build with gigabit internet?",
+                    "Why do you need better internet?"].shuffle.first
     end
 end
