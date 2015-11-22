97b275f2386f8da0b848c1002b8ebdb430603bad
Joe Pasqualetti <public_github@joepasq.com>
2015-11-22 14:41:56 -0500

    Add anti-obscenity validation with Obscenity gem

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,8 @@
+require 'obscenity/active_model'
+# http://www.rubydoc.info/gems/obscenity/1.0.2
+
 class Entry < ActiveRecord::Base
+  validates :title, obscenity: { 
+    message: "Keep it clean" 
+  }
 end
