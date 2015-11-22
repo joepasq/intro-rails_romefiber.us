c64b37e8283a280dd7a89336629de0a9fcd0d3cf
Joe Pasqualetti <public_github@joepasq.com>
2015-11-22 18:02:02 -0500

    Restrict Entries from being edited/updated, or deleted.
    - If we had accounts, or tracked authorship via cookies we could allow these actions but this is easier.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,5 @@
 class EntriesController < ApplicationController
-  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :set_entry, only: [:show] #, :edit, :update, :destroy]
 
   # GET /entries
   # GET /entries.json
@@ -18,8 +18,8 @@ class EntriesController < ApplicationController
   end
 
   # GET /entries/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /entries
   # POST /entries.json
@@ -39,27 +39,27 @@ class EntriesController < ApplicationController
 
   # PATCH/PUT /entries/1
   # PATCH/PUT /entries/1.json
-  def update
-    respond_to do |format|
-      if @entry.update(entry_params)
-        format.html { redirect_to @entry, notice: 'Entry was successfully updated.' }
-        format.json { render :show, status: :ok, location: @entry }
-      else
-        format.html { render :edit }
-        format.json { render json: @entry.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @entry.update(entry_params)
+  #       format.html { redirect_to @entry, notice: 'Entry was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @entry }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @entry.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /entries/1
   # DELETE /entries/1.json
-  def destroy
-    @entry.destroy
-    respond_to do |format|
-      format.html { redirect_to entries_url, notice: 'Entry was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @entry.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to entries_url, notice: 'Entry was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
