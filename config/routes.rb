34fec8d8d96fe34eda0d5091f9abb317d27c49c6
Joe Pasqualetti <public_github@joepasq.com>
2015-11-21 19:39:53 -0500

    Generate `entry` scaffold.
    1. rails generate scaffold entry title:string
    - rake db:migrate
    - Insert `resources :entries` into config/routes.rb
    - Insert `<p>Check out our entries at <%= link_to('Entries', entries_path) %></p>` into plain_pages/index
    - Visit localhost:3000

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :entries
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'plain_pages#index'
+  resources :entries
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
